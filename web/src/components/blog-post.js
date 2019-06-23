cbf8efbb18bf94fc15d7c06d24c5ecd7637a7817
Knut Melvær <knut.melvaer@gmail.com>
2019-06-23 09:06:49 +0200

    more wm markup

--- web/src/components/blog-post.js
+++ web/src/components/blog-post.js
@@ -1,15 +1,15 @@
 import {format, distanceInWords, differenceInDays} from 'date-fns'
 import React from 'react'
-import {buildImageObj, getBlogUrl} from '../lib/helpers'
+import {buildImageObj, getBlogUrl, toPlainText} from '../lib/helpers'
 import {imageUrlFor} from '../lib/image-url'
 import PortableText from './portableText'
 import Container from './container'
 import AuthorList from './author-list'
-
+import Bio from './bio'
 import styles from './blog-post.module.css'
 
 function BlogPost (props) {
-  const {_rawBody, authors, categories, title, mainImage, publishedAt, slug} = props
+  const {_rawBody, _rawExcerpt, authors, categories, title, mainImage, publishedAt, slug} = props
   return (
     <article className={styles.root + " h-entry"}>
       <Container>
@@ -26,10 +26,10 @@ function BlogPost (props) {
           </div>
         )}
         <div className={styles.grid}>
-          <div className={styles.mainContent}>
+          <main className={styles.mainContent}>
             <h1 className={styles.title + " p-name"}>{title}</h1>
             {_rawBody && <PortableText className="e-content" blocks={_rawBody} />}
-          </div>
+          </main>
           <aside className={styles.metaContent}>
             {publishedAt && (
               <div className={styles.publishedAt + " dt-published"}>
@@ -37,7 +37,9 @@ function BlogPost (props) {
                 {differenceInDays(new Date(publishedAt), new Date()) > 3
                   ? distanceInWords(new Date(publishedAt), new Date())
                   : format(new Date(publishedAt), 'MMMM Do, YYYY')}
-                </a><time className={styles.hidden + " dt-published"}>{publishedAt}</time>
+                </a>
+                <time className={styles.hidden + " dt-published"} itemprop="datepublished" datetime={publishedAt}>{new Date(publishedAt).toISOString().replace('Z', '') + "+01:00"}</time>
+                {_rawExcerpt && <div className={styles.hidden + " p-summary"}>{toPlainText(_rawExcerpt)}</div>}
               </div>
             )}
             {authors && <AuthorList items={authors} title='Authors' />}
@@ -52,9 +54,11 @@ function BlogPost (props) {
               </div>
             )}
             <div>
-
             </div>
           </aside>
+          <footer className={styles.footer}>
+            {authors && authors.map(({author}) => <Bio author={author} />)}
+          </footer>
         </div>
       </Container>
     </article>
--- web/src/lib/helpers.js
+++ web/src/lib/helpers.js
@@ -31,3 +31,19 @@ export function buildImageObj (source) {
 
   return imageObj
 }
+
+
+const defaults = {nonTextBehavior: 'remove'}
+
+export function toPlainText(blocks, opts = {}) {
+  const options = Object.assign({}, defaults, opts)
+  return blocks
+    .map(block => {
+      if (block._type !== 'block' || !block.children) {
+        return options.nonTextBehavior === 'remove' ? '' : `[${block._type} block]`
+      }
+
+      return block.children.map(child => child.text).join('')
+    })
+    .join('\n\n')
+}
