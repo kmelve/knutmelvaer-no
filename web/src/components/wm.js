1b4c10392175a30f59df0181e3c0cf3594120410
Knut Melvær <knut.melvaer@gmail.com>
2019-06-24 19:21:13 +0200

    add reposts

--- web/src/components/wm.js
+++ web/src/components/wm.js
@@ -2,6 +2,10 @@ import React from 'react'
 import css from './wm.module.css'
 export default function wm ({ edges }) {
   const likes = edges.filter(({ node }) => node.wmProperty === 'like-of')
+  const retweets = edges.filter(({ node }) => node.wmProperty === 'repost-of')
+  const retweetFaces = retweets.map(
+    ({ node }) => node.author && { wmId: node.wmId, ...node.author }
+  )
   const likeFaces = likes.map(
     ({ node }) => node.author && { wmId: node.wmId, ...node.author }
   )
@@ -12,6 +16,16 @@ export default function wm ({ edges }) {
       </h4>
       <div className={css.container}>
         {likeFaces.map(face => (
+          <a className={css.face} href={face.url}>
+            <img alt={face.name} src={face.photo} key={face.wmId} />
+          </a>
+        ))}
+      </div>
+      <h4>
+        <span>{`${retweets.length} reposts`}</span>
+      </h4>
+      <div className={css.container}>
+        {retweetFaces.map(face => (
           <a className={css.face} href={face.url}>
             <img alt={face.name} src={face.photo} key={face.wmId} />
           </a>
