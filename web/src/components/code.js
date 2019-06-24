b4d7c15b584b5529843a3c5f81f7af0bdc0e9263
Knut Melvær <knut.melvaer@gmail.com>
2019-06-24 23:23:58 +0200

    new markup

---
+++ web/src/components/code.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+//import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+function Code ({ node }) {
+  if(!node.code) {
+    return undefined
+  }
+  return (
+    <SyntaxHighlighter language={node.language || 'text'} showLineNumbers wrapLines>
+      {node.code}
+    </SyntaxHighlighter>
+  )
+}
+
+export default Code
--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Figure from './Figure'
-
+import Code from './Code'
 const serializers = {
+  container: ({children}) => <React.Fragment>{children}</React.Fragment>,
   types: {
     authorReference: ({node}) => <span>{node.author.name}</span>,
-    mainImage: Figure
+    mainImage: Figure,
+    code: Code
   }
 }
 
