78a88ffd067440795de57f97b4c9c45d08586dd1
Knut Melvær <knut.melvaer@gmail.com>
2020-02-02 08:24:30 -0800

    add actions

--- studio/resolveDocumentActions.js
+++ studio/resolveDocumentActions.js
@@ -1,8 +1,10 @@
 // import the default document actions
-import * as defaultActions from 'part:@sanity/base/document-actions'
+import {defaultActions} from 'part:@sanity/base/document-actions'
 
 import {PublishNowAction} from './src/actions/PublishNowAction'
 
 export default function resolveDocumentActions() {
-  return [...Object.values(defaultActions), PublishNowAction]
+  const actions = [...Object.values(defaultActions), PublishNowAction]
+  debugger
+  return actions
 }
