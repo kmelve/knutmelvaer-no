6f0839f3c8ebfe9aa8f15cbc663c05b6b413e0ac
Knut Melvær <knut.melvaer@gmail.com>
2020-02-02 11:38:25 -0800

    cleanup

--- studio/schemas/documents/category.js
+++ studio/schemas/documents/category.js
@@ -9,6 +9,14 @@ export default {
       title: 'Title'
     },
     {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      options: {
+        source: 'title'
+      }
+    },
+    {
       name: 'description',
       type: 'text',
       title: 'Description'
