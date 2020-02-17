aac9145876d002d37a546db1644f16a786c0af68
Knut Melvær <knut.melvaer@gmail.com>
2020-02-17 22:31:49 +0100

    update studio¨

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -1,11 +1,10 @@
-const { isFuture, parseISO } = require('date-fns')
+const { format, isFuture, parseISO } = require('date-fns')
 /**
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-const { format } = require('date-fns')
 
 async function createCategoryPages (graphql, actions, reporter) {
   const { createPage } = actions
