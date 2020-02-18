309c49c8604736fd081da83e65d2b8c54c127eb6
Knut Melvær <knut.melvaer@gmail.com>
2020-02-18 06:57:23 +0100

    add snazzy code styling. felt cute. might delete after

--- web/src/components/code.js
+++ web/src/components/code.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 //import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import styles from './code.module.css'
 
 function Code ({ node }) {
   if(!node.code) {
     return undefined
   }
   return (
-    <SyntaxHighlighter language={node.language || 'text'} wrapLines>
-      {node.code}
-    </SyntaxHighlighter>
+    <div className={styles.container}>
+      <SyntaxHighlighter language={node.language || 'text'} wrapLines>
+        {node.code}
+      </SyntaxHighlighter>
+    </div>
   )
 }
 
