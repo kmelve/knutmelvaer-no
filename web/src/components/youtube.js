33ea38614de59c041c153a964466e3b459d556b0
Knut Melvær <knut.melvaer@gmail.com>
2020-04-19 23:00:44 +0200

    make YT responsive

--- web/src/components/youtube.js
+++ web/src/components/youtube.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import getYouTubeId from 'get-youtube-id'
 import YouTubeEmbed from 'react-youtube'
+import css from './youtube.module.css'
 
 const YouTube = ({node}) => {
-  const { url } = node
+  const {url} = node
   const id = getYouTubeId(url)
-  return (<YouTubeEmbed videoId={id} />)
+  return (<YouTubeEmbed
+    containerClassName={css.videoWrapper}
+    videoId={id}
+  />)
 }
 
 export default YouTube
