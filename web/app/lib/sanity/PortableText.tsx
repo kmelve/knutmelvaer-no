c7680ed46209c3a1bffc2ad8e790926037a79b4e
Knut Melvær <knut.melvaer@gmail.com>
2021-11-26 18:30:41 -0800

    add portable text helper

--- web/app/lib/sanity/PortableText.tsx
+++
@@ -1,7 +0,0 @@
-import BlockContent from "@sanity/block-content-to-react";
-
-const serializers = {};
-
-export const PortableText = ({ blocks }: { blocks: any }) => {
-  return <BlockContent blocks={blocks} serializers={serializers} />;
-};
