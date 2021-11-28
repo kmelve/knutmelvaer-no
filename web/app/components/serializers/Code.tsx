ca7c81b2e585d9b0e9fd6df99c22ef94e9e972c4
Knut Melvær <knut.melvaer@gmail.com>
2021-11-27 17:02:23 -0800

    moar setup

--- web/app/components/serializers/Code.tsx
+++ web/app/components/serializers/Code.tsx
@@ -1,23 +1,23 @@
 import Refractor from "react-refractor";
-import ts from "refractor/lang/typescript";
-import js from "refractor/lang/javascript";
-import json from "refractor/lang/json";
-import jsx from "refractor/lang/jsx";
-import bash from "refractor/lang/bash";
-import css from "refractor/lang/css";
-import php from "refractor/lang/php";
-import sh from "refractor/lang/shell-session";
-import markdown from "refractor/lang/markdown";
+// import ts from "refractor/lang/typescript";
+// import js from "refractor/lang/javascript";
+// import json from "refractor/lang/json";
+// import jsx from "refractor/lang/jsx";
+// import bash from "refractor/lang/bash";
+// import css from "refractor/lang/css";
+// import php from "refractor/lang/php";
+// import sh from "refractor/lang/shell-session";
+// import markdown from "refractor/lang/markdown";
 
-Refractor.registerLanguage(js);
-Refractor.registerLanguage(ts);
-Refractor.registerLanguage(json);
-Refractor.registerLanguage(jsx);
-Refractor.registerLanguage(php);
-Refractor.registerLanguage(bash);
-Refractor.registerLanguage(sh);
-Refractor.registerLanguage(css);
-Refractor.registerLanguage(markdown);
+// Refractor.registerLanguage(js);
+// Refractor.registerLanguage(ts);
+// Refractor.registerLanguage(json);
+// Refractor.registerLanguage(jsx);
+// Refractor.registerLanguage(php);
+// Refractor.registerLanguage(bash);
+// Refractor.registerLanguage(sh);
+// Refractor.registerLanguage(css);
+// Refractor.registerLanguage(markdown);
 
 type Code = {
   code: string;
