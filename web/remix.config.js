ac58ca64575a7d4b4c3dc37cc8db09e34c3a1e26
Knut Melvær <knut.melvaer@gmail.com>
2021-11-27 17:16:30 -0800

    fix serverbuild

--- web/remix.config.js
+++ web/remix.config.js
@@ -5,6 +5,6 @@ module.exports = {
   appDirectory: "app",
   browserBuildDirectory: "public/build",
   publicPath: "/build/",
-  serverBuildDirectory: "build/api",
+  serverBuildDirectory: "api/build",
   devServerPort: 8002
 };
