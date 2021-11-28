14953d4b297c315344a04c47f6f6f5f4fe5b2951
Knut Melvær <knut.melvaer@gmail.com>
2021-11-27 17:05:23 -0800

    change code highlighting

--- web/app/components/serializers/Code.tsx
+++ web/app/components/serializers/Code.tsx
@@ -1,23 +1,5 @@
-import Refractor from "react-refractor";
-// import ts from "refractor/lang/typescript";
-// import js from "refractor/lang/javascript";
-// import json from "refractor/lang/json";
-// import jsx from "refractor/lang/jsx";
-// import bash from "refractor/lang/bash";
-// import css from "refractor/lang/css";
-// import php from "refractor/lang/php";
-// import sh from "refractor/lang/shell-session";
-// import markdown from "refractor/lang/markdown";
-
-// Refractor.registerLanguage(js);
-// Refractor.registerLanguage(ts);
-// Refractor.registerLanguage(json);
-// Refractor.registerLanguage(jsx);
-// Refractor.registerLanguage(php);
-// Refractor.registerLanguage(bash);
-// Refractor.registerLanguage(sh);
-// Refractor.registerLanguage(css);
-// Refractor.registerLanguage(markdown);
+import Highlight, { defaultProps } from "prism-react-renderer";
+import github from "prism-react-renderer/themes/github";
 
 type Code = {
   code: string;
@@ -34,12 +16,20 @@ const languageOverrides = (lang: string) =>
 export default function Code({ node }: { node: Code }) {
   const { code, language } = node;
   return (
-    <>
-      <Refractor
-        value={code}
-        language={languageOverrides(language)}
-        className="my-0"
-      />
-    </>
+    <Highlight {...defaultProps} code={code} language={language} theme={github}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            // eslint-disable-next-line react/jsx-key
+            <div {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                // eslint-disable-next-line react/jsx-key
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
   );
 }
